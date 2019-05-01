8a9aab01951c042021ca78cc495a5ae2a148f1a7
jason <122230606@qq.com>
2019-05-01 08:29:12 +0800

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/MyBlog/'
+  }
+} : {}
 
 module.exports = {
+  ...routerBase,
   plugins: [
     '~/plugins/vue-markdown',
     '~/plugins/element-ui'
